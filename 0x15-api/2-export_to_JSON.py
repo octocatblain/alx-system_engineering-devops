0e725ff7d42197973a9afb41fa5694480d44616a
Blain Muema <blainmasaku2@gmail.com>
2024-01-02 14:04:29 +0000

    Task 3. export to json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -6,32 +6,14 @@ import sys
 
 if __name__ == "__main__":
     user_id = sys.argv[1]
-
     url = "https://jsonplaceholder.typicode.com/"
-    
-    user_response = requests.get(url + "users/{}".format(user_id))
-    user_data = user_response.json()
-
-    if "id" not in user_data:
-        print("Error: User not found.")
-        sys.exit(1)
-
-    username = user_data.get("username")
-
-    todos_response = requests.get(url + "todos", params={"userId": user_id})
-    todos_data = todos_response.json()
-
-    user_tasks = {
-        user_id: [
-            {
-                "task": task.get("title"),
-                "completed": task.get("completed"),
-                "username": username
-            } for task in todos_data
-        ]
-    }
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
     with open("{}.json".format(user_id), "w") as jsonfile:
-        json.dump(user_tasks, jsonfile)
-
-    print("JSON file exported successfully for USER_ID: {}".format(user_id))
+        json.dump({user_id: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": username
+            } for t in todos]}, jsonfile)
