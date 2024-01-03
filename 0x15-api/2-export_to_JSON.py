7c68465b94d7d13d4eb09034e4db46b4c76d2cdf
Blain Muema <blainmasaku2@gmail.com>
2024-01-03 12:28:44 +0000

    correction

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
-""" Script Exports to-do list information for a given employee ID to JSON format."""
+"""
+Fetch and Export to JSON employee TODO list progress using a REST API.
+"""
 import json
 import requests
 import sys
 
-if __name__ == "__main__":
-    user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    with open("{}.json".format(user_id), "w") as jsonfile:
+def export_to_json():
+    """return API data"""
+    base_url = 'https://jsonplaceholder.typicode.com/'
+    user_id = sys.argv[1]
+    user = requests.get(base_url + f"users/{user_id}").json()
+    todos = requests.get(base_url + f"users/{user_id}/todos").json()
+
+    """Export data to JSON"""
+    with open(f"{user_id}.json", 'w') as jsonfile:
         json.dump({user_id: [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": username
-            } for t in todos]}, jsonfile)
+            "task": todo.get('title'),
+            "completed": todo.get('completed'),
+            "username": user.get('username')
+        }for todo in todos]}, jsonfile)
+
+
+if __name__ == '__main__':
+    export_to_json()
